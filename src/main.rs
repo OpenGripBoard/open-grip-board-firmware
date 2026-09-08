86d419e4cc1284bf785a7a24ff715908136b1a1e
Jonas Burkhard <jonas@burkhard.one>
2026-09-08 18:08:23 +0200

    feat: add starter template

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use std::{thread::sleep, time::Duration};
+
+fn main() {
+    // It is necessary to call this function once. Otherwise, some patches to the runtime
+    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
+    esp_idf_svc::sys::link_patches();
+
+    // Bind the log crate to the ESP Logging facilities
+    esp_idf_svc::log::EspLogger::initialize_default();
+
+    while true{
+        log::info!("Hello, world!");
+        sleep(Duration::from_millis(1000));
+    }
+    
+}
